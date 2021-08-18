4abd02706808fbc4be2a08dc7bd4824b8169bf59
Mario Taddeucci <mariotaddeucci@gmx.com>
2021-08-18 13:59:44 -0300

    Fixed connections yaml

--- image/script/start.py
+++ image/script/start.py
@@ -1,5 +1,5 @@
 from subprocess import Popen
-from os import path, rename
+from os import path
 import yaml
 
 
@@ -7,7 +7,7 @@ def load_secrets():
     if not path.exists("connections.yaml"):
         return
 
-    with open("secrets.yaml", "r") as stream:
+    with open("connections.yaml", "r") as stream:
         for conn_id, config in yaml.safe_load(stream).items():
             if isinstance(config, str):
                 yield f"{conn_id} --conn-uri {config}"
@@ -25,10 +25,8 @@ def load_secrets():
 if path.exists("requirements.txt"):
     Popen("pip install -r requirements.txt", shell=True).wait()
 
-# Add connections
-if path.exists("connections.yml"):
-    rename("connections.yml", "connections.yaml")
-processes = [Popen(f"airflow connections add {c}", shell=True) for c in load_secrets()]
+processes = [
+    Popen(f"airflow connections add {c}", shell=True) for c in load_secrets()]
 [p.wait() for p in processes]
 
 # Start Airflow
